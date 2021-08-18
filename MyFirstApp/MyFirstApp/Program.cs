c19ec34a28422d16ada096b50666ec4ad02d64d8
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2021-08-18 11:17:17 +0200

    Revert "sync"
    This reverts commit d1176eaf721cc73bdaebf9b74043010bedb62e89.

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! My name is Viktor Rådberg 2.");
+            Console.WriteLine("Hello! My name is Viktor Rådberg.");
         }
     }
 }
